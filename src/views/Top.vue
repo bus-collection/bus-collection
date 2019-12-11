530aeb7f9dcccf2ef6e37f061db700505a6bf79a
shunsuke toba <boushi_sukaimu@yahoo.co.jp>
2019-12-11 18:38:37 +0900

    fix(top): トップを画面全体にする

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,17 +1,38 @@
 <template>
-  <div class="top">
+  <div class="top" @click="toQuestList">
     <h1>バスコレ</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "top"
+  name: "top",
+  methods: {
+    toQuestList() {
+      this.$router.push("/quest-list");
+    }
+  }
 };
 </script>
 
 <style scoped>
+h1, .top {
+  margin: 0;
+  padding: 0;
+}
+
 .top {
+  height: 100vh;
+  width: 100vw;
   background: radial-gradient(#ff0000, #ff0099);
+  display: flex;
+  align-items: center;
+  vertical-align: middle;
+}
+
+h1 {
+  color: orange;
+  font-size: 5rem;
+  margin: 0 auto;
 }
 </style>
