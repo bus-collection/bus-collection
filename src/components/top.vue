ef27391c4138be0cd3e5d3611f58f6e624991be0
W-Nakasone <psyqhical@icloud.com>
2019-12-11 16:30:59 +0900

    最初とその次のページ

---
+++ src/components/top.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top">
+    
+    
+    <h1>バスコレ</h1>
+ 
+
+  
+  </div>
+</template>
+<script>
+
+</script>>
+
+<style scoped>
+.top {
+   background: radial-gradient(#FF0000,#FF0099);
+ 
+}
+
+</style>
